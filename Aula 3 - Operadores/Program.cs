0e4d78e4be95a6dc5164889224b2adfd041dd962
FRANCINE BEATRIZ SOUSA DA SILVA <francine.bssilva@senacsp.edu.br>
2025-11-03 20:43:42 -0300

    primeiro commit com codigo

---
+++ Aula 3 - Operadores/Program.cs
@@ -0,0 +1,23 @@
+﻿//1
+
+Console.WriteLine("Escreva dois números inteiros.");
+string número1 = Console.ReadLine();
+string número2 = Console.ReadLine();
+Console.WriteLine(número1);
+Console.Write(número2);
+
+
+//2
+Console.WriteLine(número1 + número2);
+int.Parse(número1 = Console.ReadLine());
+int.Parse(número2 = Console.ReadLine());
+Console.WriteLine(número1 + número2);
+
+//3
+Console.WriteLine("Escreva dois números inteiros.");
+string número3 = Console.ReadLine();
+string número4 = Console.ReadLine();
+int.Parse(número3 = Console.ReadLine());
+int.Parse(número4 = Console.ReadLine());
+
+Console.WriteLine(int.Parse(número3 = Console.ReadLine()) - int.Parse(número4 = Console.ReadLine());
