43b05d6bf1e0f6a5fefebef13f27069b0a8f5afb
FRANCINE BEATRIZ SOUSA DA SILVA <francine.bssilva@senacsp.edu.br>
2025-11-05 21:58:43 -0300

    Aula 5 revisão

---
+++ Aula 5 - Revisão/Program.cs
@@ -0,0 +1,42 @@
+﻿//Peça para o usuário inserir dois números inteiros e mostre-os no Console.
+
+using System;
+
+Console.WriteLine("Insira um numero inteiros");
+int n1;
+int n2;
+n1 = int.Parse(Console.ReadLine());
+
+n2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("seu primero numero é  " + n1);
+Console.WriteLine("seu segundo numero é  " + n2);
+
+Console.WriteLine("---------------------------");
+
+
+Console.WriteLine("Insira um número Inteiro");
+int N3;
+N3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira um segundo número inteiro");
+int N4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("A soma dos dois valores é");
+Console.WriteLine(N3 + N4);
+
+Console.WriteLine("--------------------------------");
+
+
+Console.WriteLine("Insira um número inteiro");
+
+int N5 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira um segundo número inteiro");
+Double N6 = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("A subtração dos dois valores é");
+Console.WriteLine(N5 - N6);
+
+
+
